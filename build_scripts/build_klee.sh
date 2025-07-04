e2b9d187a83b17a702e1d73ca45efcc6f44fb268
zz-fz-john <516193608@qq.com>
2025-07-04 09:28:46 +0800

    complete klee and to build llvm

--- build_scripts/build_klee.sh
+++ build_scripts/build_klee.sh
@@ -25,22 +25,46 @@ git submodule init && git submodule update
 ./scripts/deps/setup-minisat.sh
 mkdir build
 cd build
+# use python3
+## add comment in stp/tools/stp_simple/CMakeLists.txt
+#修改 target_link_libraries(stp_simple-bin  stp)为 target_link_libraries(stp_simple-bin PUBLIC stp)
+## 并在该行后添加
+    # find_library(DL_LIBRARY dl)
+    # if(DL_LIBRARY)
+    #     target_link_libraries(stp_simple-bin PUBLIC ${DL_LIBRARY})
+    # endif()
+#修改 stp/tools/stp/CMakeLists.txt
+#修改 target_link_libraries(stp-bin  stp Boost::program_options)为 target_link_libraries(stp-bin PUBLIC stp Boost::program_options)
+#并在该行后添加
+    # find_library(DL_LIBRARY dl)
+    # if(DL_LIBRARY)
+    #     target_link_libraries(stp-bin PUBLIC ${DL_LIBRARY})
+    # endif()
 cmake ..
 cmake --build .
 export STP_DIR=/home/zrz0517/study/chain_attestation/OP-DFI/toolchain/stp/build
 export LD_LIBRARY_PATH="$STP_DIR/lib:$LD_LIBRARY_PATH"
 
 
-#build libc++
+#build libc++ ，use gcc-7 and do :pip3 install wllvm
 cd toolchain
 LLVM_VERSION=13 BASE=/home/zrz0517/study/chain_attestation/OP-DFI/toolchain/LIBCXX_DIR ENABLE_OPTIMIZED=1 DISABLE_ASSERTIONS=1 ENABLE_DEBUG=0 REQUIRES_RTTI=1 klee/scripts/build/build.sh libcxx
 
 #finally, build klee
+#在klee/CMakeLists.txt中修改
+#在if (${HAVE_GPERFTOOLS_MALLOC_EXTENSION_H})前添加
+  # if (EXISTS "${TCMALLOC_INCLUDE_DIR}/gperftools/malloc_extension.h")
+  #   set(HAVE_GPERFTOOLS_MALLOC_EXTENSION_H TRUE)
+  # else()
+  #   set(HAVE_GPERFTOOLS_MALLOC_EXTENSION_H FALSE)
+  # endif()
+#sudo apt install libsqlite3-dev
+#pip3 install lit
 cd toolchain/klee
 mkdir build
 cd build
 cmake -DENABLE_SOLVER_STP=ON -DENABLE_POSIX_RUNTIME=ON -DKLEE_UCLIBC_PATH=/home/zrz0517/study/chain_attestation/OP-DFI/toolchain/klee-uclibc \
- -DENABLE_UNIT_TESTS=ON  -DENABLE_KLEE_LIBCXX=ON \
+ -DENABLE_UNIT_TESTS=OFF  -DENABLE_KLEE_LIBCXX=ON \
   -DKLEE_LIBCXX_DIR=/home/zrz0517/study/chain_attestation/OP-DFI/toolchain/LIBCXX_DIR/libc++-install-130 \
  -DKLEE_LIBCXX_INCLUDE_DIR=/home/zrz0517/study/chain_attestation/OP-DFI/toolchain/LIBCXX_DIR/libc++-install-130/include/c++/v1 \
  -DENABLE_KLEE_EH_CXX=ON -DKLEE_LIBCXXABI_SRC_DIR=/home/zrz0517/study/chain_attestation/OP-DFI/toolchain/LIBCXX_DIR/llvm-130/libcxxabi \
--- build_scripts/build_llvm.sh
+++ build_scripts/build_llvm.sh
@@ -3,8 +3,11 @@ git clone  https://github.com/llvm/llvm-project
 cd llvm-project
 git checkout 75e33f71c2dae584b13a7d1186ae0a038ba98838
 git apply ../../LLVM_patch/*
-
+#修改/llvm/lib/Transforms/code_switch_llvmpasses/helper_information_log/CMakeLists.txt
+# include_directories(/home/opdfi/toolchain/json/include)
+# 改为：include_directories(/home/zrz0517/study/chain_attestation/OP-DFI/toolchain/llvm-project/external/nlohmann/)
+#use gcc-7
 mkdir build
 cd build
-cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="lld;llvm;clang" -DLLVM_TARGETS_TO_BUILD="all" -DLLVM_ENABLE_LTO=ON ../llvm
+cmake -G "Unix Makefiles" -DLLVM_ENABLE_PROJECTS="lld;llvm;clang" -DLLVM_TARGETS_TO_BUILD=ARM  -DLLVM_ENABLE_LTO=ON ../llvm
 make -j2
